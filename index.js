d9873575fd01586ada5fb3b9b7a1ea6201ce1fa9
nodi-andy <andy@doganmail.de>
2023-02-06 00:40:53 +0000

    subdomain

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import vhost from 'vhost';
+
 var app = express();
 
 app.use("/", express.static("./public"));
+app.use(vhost('factory.mynodi.com/', express.static('./public/browser-factory')))
+app.use(vhost('*.noditron.com/', express.static('./public/browser-factory')))
 
 app.listen(8080)
